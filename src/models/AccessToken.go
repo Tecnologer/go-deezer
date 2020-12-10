7803afbbfbdef9eb6ed8250fcb11ddb0de97b94d
Rey David <rdominguez@tecnologer.net>
2020-12-09 22:06:46 -0600

    implement get token and search artist

--- src/models/AccessToken.go
+++ src/models/AccessToken.go
@@ -1,7 +1,16 @@
 package models
 
+import "time"
+
 //AcessToken for response of access token auth
 type AcessToken struct {
-	Token   string `json:"access_token"`
-	Expires int    `json:"expires"`
+	Token     string `json:"access_token"`
+	Expires   int    `json:"expires"`
+	CreatedAt time.Time
+}
+
+//IsTokenExpired returns true if the Expires time has completed
+func (t *AcessToken) IsTokenExpired() bool {
+	duration := time.Since(t.CreatedAt)
+	return int(duration*time.Second) > t.Expires-60
 }
