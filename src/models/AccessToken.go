d8e7174473f058dbf701843018872f5fedb417e4
Rey David <rdominguez@tecnologer.net>
2020-12-13 18:24:56 -0600

    access token jsno directive for created at

--- src/models/AccessToken.go
+++ src/models/AccessToken.go
@@ -4,9 +4,9 @@ import "time"
 
 //AcessToken for response of access token auth
 type AcessToken struct {
-	Token     string `json:"access_token"`
-	Expires   int    `json:"expires"`
-	CreatedAt time.Time
+	Token     string    `json:"access_token"`
+	Expires   int       `json:"expires"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 //IsTokenExpired returns true if the Expires time has completed
